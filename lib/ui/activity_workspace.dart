1ec94c7832f032cbd581a8161b905f27320b4ea2
wellorbetter <1419919418@qq.com>
2026-08-27 16:51:51 +0800

    fix: use Dart null-aware element syntax

--- lib/ui/activity_workspace.dart
+++ lib/ui/activity_workspace.dart
@@ -344,7 +344,7 @@ class ActivityWorkspace extends StatelessWidget {
                     ],
                   ),
                 ),
-                trailing?,
+                ?trailing,
               ],
             ),
             const SizedBox(height: 18),
